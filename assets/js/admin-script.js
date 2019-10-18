bb627db755a7102307d29f5aa70cbf58e700e9df
Shishta <shishta@codepixelz.com.np>
2019-10-18 16:20:24 +0545

    Script issues resolved

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -1,25 +1,21 @@
-jQuery(document).ready( function() {
+(function ($) {
+    $(function () {
 
-    //initialize tab on backend
-    jQuery('#tabs-wrap').tabs();
+        //initialize tab on backend
+        $('#tabs-wrap').tabs();
 
-    // jQuery( "#accordion" ).accordion({
-    //     header: '> div.wrap > h3',
-    //     collapsible: true
-    // });
-
-    $(".acc-item > a").on("click", function(e) {
-        e.preventDefault();
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active");
-            $(this).siblings(".acc-content").slideUp(250);
-        } else {
-            $(".acc-item > a").removeClass("active");
-            $(this).addClass("active");
-            $(".acc-content").slideUp(250);
-            $(this).siblings(".acc-content").slideDown(250);
-        }
-    });
-
-});
+        $(".acc-item > a").on("click", function(e) {
+            e.preventDefault();
+            if ($(this).hasClass("active")) {
+                $(this).removeClass("active");
+                $(this).siblings(".acc-content").slideUp(250);
+            } else {
+                $(".acc-item > a").removeClass("active");
+                $(this).addClass("active");
+                $(".acc-content").slideUp(250);
+                $(this).siblings(".acc-content").slideDown(250);
+            }
+        });
+    }); // END OF DOCUMENT READY
+})(jQuery);
 
